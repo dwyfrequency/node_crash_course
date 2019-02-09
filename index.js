9a1ee3f2dd52dd525d400cfad708b1b7970ef5d8
dwyfrequency <jdwy215@gmail.com>
2019-02-09 13:48:33 -0500

    adding notes for path

---
+++ index.js
@@ -0,0 +1,4 @@
+// const person = require("./person");
+const Person = require("./person");
+const person1 = new Person("Tom", 23);
+person1.greeting();
---
+++ reference/path_demo.js
@@ -0,0 +1,13 @@
+const path = require("path");
+
+// Base file name : path_demo.js
+console.log(path.basename(__filename));
+
+// Directory name : */sideProjs/node_crash_course/reference
+console.log(path.dirname(__filename));
+
+// File extension : .js
+console.log(path.extname(__filename));
+
+// Create path object : object of root, dir, base, ext, and name
+console.log(path.parse(__filename));
